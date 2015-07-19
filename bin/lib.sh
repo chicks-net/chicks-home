e1c0738b4d01bf34856d0bef60399b0cb5388e83
Christopher Hicks <chicks.net@gmail.com>
2015-07-19 06:07:18 -0700

    add Ed's spinner for http://stackoverflow.com/questions/31499562/simulating-a-spinner-for-progress-in-bash :mortar_board:

---
+++ bin/lib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# chicks' shell library
+#
+# https://github.com/chicks-net/chicks-home/tree/master/bin/lib.sh
+#
+
+spinner() {
+	local pid=$1
+	local delay=0.75
+	local spinstr='|/-\'
+	while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+		local temp=${spinstr#?}
+		printf " [%c]  " "$spinstr"
+		local spinstr=$temp${spinstr%"$temp"}
+		sleep $delay
+		printf "\b\b\b\b\b\b"
+	done
+	printf "    \b\b\b\b"
+}
