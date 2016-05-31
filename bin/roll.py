4a2fc1ccc274e75346ce524565a93f81d2e69144
Christopher Hicks <chicks.net@gmail.com>
2016-05-31 09:55:49 -0700

    finish roll in python :game_die:

--- bin/roll.py
+++ bin/roll.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""roll D&D dice"""
 
 from sys import argv
 from random import randrange
@@ -7,22 +8,23 @@ import re
 if len(argv) < 2:
 	raise ValueError("args are dice in D&D format like 2d6")
 
-args = argv[1:]
-
-for roll in args:
-	# TODO: handle missing count
-	matched = re.match(r'^(\d+)d(\d+)$',roll)
+for roll in argv[1:]:
+	matched = re.match(r'^(\d*)d(\d+)$', roll)
 	if matched:
 		print roll + ':'
-		count = int( matched.group(1) )
+		roll_sum = 0
+		count = 0
+		if len( matched.group(1) ):
+			count = int( matched.group(1) )
+		else:
+			count = 1
 		sides = int( matched.group(2) )
-		sum = 0
 		for z in range(count):
-			rolled = randrange(1,sides+1)
+			rolled = randrange(1, sides+1)
 			print "\troll " + str(z) + ': ' + str(rolled)
-			sum += rolled
+			roll_sum += rolled
 
 		possible = str( sides * count )
-		print "\ttotal: " + str(sum) + "/" + possible
+		print "\ttotal: " + str(roll_sum) + "/" + possible
 	else:
 		print roll + ' is not a dice in 3d8 format'
