2efc6bb0b35d694e90f90a9320e1772454a493a0
Christopher Hicks <chicks.net@gmail.com>
2016-08-30 12:30:18 -0700

    CHM t-shirt translator translated into go :arrow_forward:

---
+++ bin/chm-tshirt.go
@@ -0,0 +1,25 @@
+// #!/usr/bin/go run
+
+// translate Computer History Museum t-shirt
+// http://i.ebayimg.com/images/g/qksAAOSwaB5XjsI1/s-l300.jpg
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var binaryStrings [3]string
+	binaryStrings = [3]string{"01000011","01001000","01001101"}
+
+	for _,bin := range binaryStrings {
+		if decimal, err := strconv.ParseInt(bin, 2, 64); err != nil {
+			fmt.Println(err)
+		} else {
+			letter := strconv.Itoa(int(decimal))
+			fmt.Println(bin, decimal, letter, string(decimal))
+		}
+	}
+}
