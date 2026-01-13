cb51dd76ab8fa2cb0c934ace475ba814bbf8b4ed
Christopher Hicks <chicks.net@gmail.com>
2026-01-13 08:36:03 -0800

    fix for shfmt

--- bin/labelgrep.sh
+++ bin/labelgrep.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 find . -name "*.lbx" -print0 | while IFS= read -r -d '' file; do
-	if zipgrep "$1" "$file" > /dev/null
-	then
+	if zipgrep "$1" "$file" > /dev/null; then
 		echo "=== $file contains $1 ==="
 	fi
 done
