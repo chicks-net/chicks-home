66a8015b79c0487ae5fae313b0260b3c9442b5a8
Christopher Hicks <chicks.net@gmail.com>
2026-01-13 08:37:30 -0800

    check arguments

--- bin/labelgrep.sh
+++ bin/labelgrep.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ "$#" -eq 0 ]; then
+	echo "Usage: $0 <pattern>" >&2
+	exit 1
+fi
+
 find . -name "*.lbx" -print0 | while IFS= read -r -d '' file; do
 	if zipgrep "$1" "$file" > /dev/null; then
 		echo "=== $file contains $1 ==="
